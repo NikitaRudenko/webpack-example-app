2d0d95f23d57d285e7cac51e4cde690806025762
ngrudenko <ngrudenko@yamoney.ru>
2018-03-04 02:49:42 +0300

    fix

--- webpack/webpack.utils.js
+++ webpack/webpack.utils.js
@@ -1,8 +1,8 @@
 const {resolve} = require('path');
 
-const publicDir = resolve(__dirname, './public');
-const distDir = resolve(__dirname, './public/dist');
-const srcDir = resolve(__dirname, './src');
+const publicDir = resolve(__dirname, '../public');
+const distDir = resolve(__dirname, '../public/dist');
+const srcDir = resolve(__dirname, '../src');
 
 module.exports = {
 	publicDir,
